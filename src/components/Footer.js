bfb7b8e13934f803df6d88a3168a04793363c394
karthickram286 <karthick.r@zohocorp.com>
2019-12-15 23:15:13 +0530

    sidebar and footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,25 @@
 import React from 'react'
+import { 
+    FaHackerrank
+} from 'react-icons/fa';
 
 class Footer extends React.Component {
     render() {
         return (
             <div id="footer">
                 <div className="inner">
+                    <h5>Find me on various platforms</h5>
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/karthick-ram/" className="icon fa-linkedin-square"><span className="label">LinkedIn</span></a></li>
+                        <li><a href="https://twitter.com/Karthic87552474" className="icon fa-twitter-square"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/karthickram286/" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="https://stackoverflow.com/users/6056634/karthick-ram" className="icon fa-stack-overflow"><span className="label">StackOverflow</span></a></li>
+                        <li><a href="https://medium.com/@karthick_ram96" className="icon fa-medium"><span className="label">Medium</span></a></li>
+                        <li><a href="https://www.hackerrank.com/Karthick_ram"><FaHackerrank></FaHackerrank></a></li>
+                        <li><a href="https://www.xing.com/profile/Karthick_Ramachandran3/cv" className="icon fa-xing-square"><span className="label">Xing</span></a></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>Made with ❤️ in Chennai</li>
                     </ul>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+                    <a href="#" className="image avatar"><img src="https://media.licdn.com/dms/image/C5103AQFKD5Za66TbkA/profile-displayphoto-shrink_200_200/0?e=1581552000&v=beta&t=dmLrmxuepUKCPb_Vu4ZvfeXWgLu4gibULm17iOFOlGg" /></a>
+                    <br />
+                    <h1><strong>Hi, this is <i>Karthick R</i><br />
+                    a Fullstack developer<br /> 
+                    based on Chennai</strong></h1>
                 </div>
                 <Footer />
             </header>

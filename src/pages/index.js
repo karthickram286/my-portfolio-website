98cbb0630e4f13e7dbedc29c4129b7bb064ac66d
karthickram286 <karthick.r@zohocorp.com>
2019-12-17 12:42:24 +0530

    contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,8 @@ class HomeIndex extends React.Component {
                         <p>Have a question for me? Hire me? Want to grab a cup of coffee? Feel free to contact me!</p>
                         <div className="row">
                             <div className="8u 12u$(small)">
-                                <form name="contact-form" method="POST" data-netlify="true" >
+                                <form name="contact-form" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+                                    <input type="hidden" name="contact-form-name" value="contact" />
                                     {/* <div className="row uniform 50%"> */}
                                         {/* <div className="6u 12u$(xsmall)"> */}
                                             <input type="text" name="name" id="name" placeholder="Name" /> <br />
